d8988df51ad43ebf19c79a1574d54556615fe1b3
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-09-02 20:07:46 +1000

    tests for delivering web ui static content and pages

--- backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/web/WebConfig.kt
+++ backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/web/WebConfig.kt
@@ -1,8 +1,18 @@
 package io.orangebuffalo.accounting.simpleaccounting.web
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.resources
+import org.springframework.web.reactive.function.server.ServerResponse
 
 @Configuration
 @EnableWebFlux
-class WebConfig
+class WebConfig {
+    @Bean
+    fun staticResourceRouter(): RouterFunction<ServerResponse> {
+        return resources("/**", ClassPathResource("META-INF/resources/"))
+    }
+}
---
+++ backend/src/test/kotlin/io/orangebuffalo/accounting/simpleaccounting/web/ui/WebUiControllerTest.kt
@@ -0,0 +1,78 @@
+package io.orangebuffalo.accounting.simpleaccounting.web.ui
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.http.MediaType.TEXT_HTML
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+internal class WebUiControllerTest {
+
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
+
+    lateinit var client: WebTestClient
+
+    @BeforeEach
+    fun setup() {
+        client = WebTestClient.bindToApplicationContext(applicationContext).build()
+    }
+
+    @Test
+    fun `Should serve static resources from root without authentication`() {
+        client.get().uri("/favicon.ico")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .consumeWith {
+                    assertThat(it.responseBody).isNotEmpty()
+                }
+    }
+
+    @Test
+    fun `Should serve css without authentication`() {
+        client.get().uri("/static/css/some.css")
+                .exchange()
+                .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `Should serve js without authentication`() {
+        client.get().uri("/js/some.js")
+                .exchange()
+                .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `Should serve admin page without authentication`() {
+        client.get().uri("/admin")
+                .accept(TEXT_HTML)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody<String>()
+                .consumeWith {
+                    assertThat(it.responseBody).isNotBlank()
+                }
+    }
+
+    @Test
+    fun `Should serve user page without authentication`() {
+        client.get().uri("/user")
+                .accept(TEXT_HTML)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody<String>()
+                .consumeWith {
+                    assertThat(it.responseBody).isNotBlank()
+                }
+    }
+
+}
