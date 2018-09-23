7adb2a8fce91e0b2cae9538079e6c97648da94bb
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-09-23 12:12:22 +1000

    implementing jwt token validation and parsing

--- backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/security/JwtService.kt
+++ backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/security/JwtService.kt
@@ -1,8 +1,9 @@
 package io.orangebuffalo.accounting.simpleaccounting.services.security
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.*
 import io.jsonwebtoken.security.Keys
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 
@@ -17,6 +18,24 @@ class JwtService {
                 .claim("authorities", userDetails.authorities.map { it.authority })
                 .signWith(keyPair.private)
                 .compact()
+    }
+
+    fun validateTokenAndBuildUserDetails(token: String): UserDetails {
+        val jws: Jws<Claims>
+        try {
+            jws = Jwts.parser()
+                    .setSigningKey(keyPair.public)
+                    .parseClaimsJws(token)
+
+        } catch (ex: JwtException) {
+            throw BadCredentialsException("Bad token $token", ex)
+        }
+
+        return User.builder()
+                .username(jws.body.subject)
+                .password(token)
+                .authorities(*(jws.body["authorities"] as List<String>).toTypedArray())
+                .build()
     }
 
 }
--- backend/src/test/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/security/JwtServiceTest.kt
+++ backend/src/test/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/security/JwtServiceTest.kt
@@ -3,8 +3,12 @@ package io.orangebuffalo.accounting.simpleaccounting.services.security
 import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.userdetails.User
 import java.util.*
+
+const val BAD_TOKEN = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJGcnkiLCJhdXRob3JpdGllcyI6WyJST0xFX0RFTElWRVJZX0JPWSJdfQ.invalid"
 
 class JwtServiceTest {
 
@@ -28,5 +32,38 @@ class JwtServiceTest {
 
         assertThatJson(jwtBody)
                 .node("authorities").isArray.containsExactlyInAnyOrder("ROLE_DELIVERY_BOY")
+    }
+
+    @Test
+    fun `Should parse the previously built token`() {
+        val jwtService = JwtService()
+
+        val user = User.builder()
+                .username("Fry")
+                .roles("DELIVERY_BOY")
+                .password("")
+                .build()
+        val token = jwtService.buildJwtToken(user)
+
+        val userDetails = jwtService.validateTokenAndBuildUserDetails(token)
+
+        assertThat(userDetails).isNotNull
+        assertThat(userDetails.username).isEqualTo("Fry")
+        assertThat(userDetails.authorities.map { it.authority }).containsExactlyInAnyOrder("ROLE_DELIVERY_BOY")
+        assertThat(userDetails.password).isEqualTo(token)
+        assertThat(userDetails.isAccountNonExpired).isTrue()
+        assertThat(userDetails.isAccountNonLocked).isTrue()
+        assertThat(userDetails.isCredentialsNonExpired).isTrue()
+        assertThat(userDetails.isEnabled).isTrue()
+    }
+
+    @Test
+    fun `Should fail on invalid token`() {
+        val jwtService = JwtService()
+
+        val actualException = assertThrows<BadCredentialsException> {
+            jwtService.validateTokenAndBuildUserDetails(BAD_TOKEN)
+        }
+        assertThat(actualException.message).isEqualTo("Bad token $BAD_TOKEN")
     }
 }
