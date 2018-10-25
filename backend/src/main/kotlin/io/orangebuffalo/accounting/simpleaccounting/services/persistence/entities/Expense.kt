5f7cf918b40d0801b83dc1ed6dd8d0615dea8d9c
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-10-26 09:14:31 +1100

    creating expense entity

---
+++ backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/persistence/entities/Expense.kt
@@ -0,0 +1,50 @@
+package io.orangebuffalo.accounting.simpleaccounting.services.persistence.entities
+
+import java.time.ZonedDateTime
+import javax.persistence.*
+
+@Entity
+class Expense(
+
+    @field:ManyToOne(optional = false)
+    @field:JoinColumn(nullable = false, foreignKey = ForeignKey(name = "expense_category_fk"))
+    var category: Category,
+
+    @field:Column(nullable = false)
+    val dateRecorded: ZonedDateTime,
+
+    @field:Column(nullable = false)
+    var datePaid: ZonedDateTime,
+
+    @field:Column(nullable = false, length = 3)
+    var currency: String,
+
+    @field:Column(nullable = false)
+    var originalAmount: Long,
+
+    @field:Column(nullable = false)
+    var amountInDefaultCurrency: Long,
+
+    @field:Column(nullable = false)
+    var actualAmountInDefaultCurrency: Long,
+
+    @field:ManyToMany
+    @field:JoinTable(
+        name = "expense_attachments",
+        foreignKey = ForeignKey(name = "expense_attachments_expense_fk"),
+        inverseForeignKey = ForeignKey(name = "expense_attachments_document_fk"),
+        inverseJoinColumns = [JoinColumn(name = "document_id")]
+    )
+    val attachments: MutableList<Document>,
+
+    @field:Column(nullable = false)
+    var percentOnBusinessInBps: Int,
+
+    @field:Column(length = 1024)
+    var notes: String?,
+
+    @field:ManyToOne(optional = false)
+    @field:JoinColumn(nullable = false, foreignKey = ForeignKey(name = "expense_workspace_fk"))
+    val workspace: Workspace
+
+) : AbstractEntity()
---
+++ backend/src/main/kotlin/io/orangebuffalo/accounting/simpleaccounting/services/persistence/repos/ExpenseRepository.kt
@@ -0,0 +1,5 @@
+package io.orangebuffalo.accounting.simpleaccounting.services.persistence.repos
+
+import io.orangebuffalo.accounting.simpleaccounting.services.persistence.entities.Expense
+
+interface ExpenseRepository : AbstractEntityRepository<Expense>
