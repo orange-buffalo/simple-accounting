3e0a43ed65fdd14da85ae479ef684d0dcd7f8613
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-11-13 09:23:14 +1100

    improving document upload component

--- frontend/src/app/components/DocumentUpload.vue
+++ frontend/src/app/components/DocumentUpload.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
     <el-upload
-        drag
+        :drag="!filePresent"
         :show-file-list="false"
         :data="uploadRequest"
         :action="`/api/v1/user/workspaces/${workspaceId}/documents`"
         :on-success="onSuccess"
         :on-error="onError"
+        :on-progress="onProgress"
+        :on-change="onChange"
+        ref="upload"
+        :disabled="filePresent"
+        :auto-upload="false"
         :headers="headers">
-      <i class="el-icon-upload"></i>
-      <div class="el-upload__text">
-        <div>Drop file here or <em>click to upload</em></div>
-        <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+      <div v-if="!filePresent">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          <div>Drop file here or <em>click to upload</em></div>
+          <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+        </div>
+      </div>
+
+      <div v-if="filePresent" class="doc-upload-file-panel">
+        <i class="el-icon-document"></i>
+        <div class="el-upload__text">
+          <!--todo display in localized kb/mb units-->
+          <div>{{selectedFile.name}} ({{selectedFile.size}})</div>
+          <div class="el-upload__tip">
+            <el-button type="text" @click="onRemove">Remove</el-button>
+          </div>
+        </div>
       </div>
     </el-upload>
     <el-input placeholder="Additional notes..." v-model="uploadRequest.notes"></el-input>
@@ -19,37 +37,20 @@
 </template>
 
 <script>
-  import api from '@/services/api'
   import {mapState, mapGetters} from 'vuex'
 
   export default {
     name: 'DocumentUpload',
 
-    props: {
-      // apiPath: {
-      //   type: String,
-      //   required: true
-      // },
-
-
-    },
+    props: {},
 
     data: function () {
-      // return {
-      //   totalElements: 0,
-      //   data: []
-      // }
       return {
         uploadRequest: {
           notes: ""
-        }
+        },
+        selectedFile: null
       }
-    },
-
-    created: function () {
-      // this.currentPage = 1
-      // this.pageSize = 10
-      // this.reloadData()
     },
 
     methods: {
@@ -64,6 +65,28 @@
           message: 'Upload failed',
           type: 'error'
         });
+      },
+
+      onProgress: function (event) {
+        console.log(event)
+      },
+
+      onChange: function (file) {
+        console.log(file)
+        if (!this.filePresent) {
+          this.selectedFile = file
+        }
+      },
+
+      onRemove: function () {
+        this.selectedFile = null
+        if (!this.uploadRequest.notes) {
+          this.$emit('on-clear')
+        }
+      },
+
+      upload: function () {
+        this.$refs.upload.submit()
       }
     },
 
@@ -77,7 +100,49 @@
         return {
           'Authorization': `Bearer ${this.bearerToken}`
         }
+      },
+
+      filePresent: function () {
+        return this.selectedFile != null
       }
     }
   }
 </script>
+
+<style lang="scss">
+  @import "~element-ui/packages/theme-chalk/src/upload";
+
+  .doc-upload-file-panel {
+    @extend .el-upload-dragger;
+    border: solid 1px #dcdfe6;
+    cursor: initial;
+
+    &:hover {
+      border: solid 1px #dcdfe6;
+    }
+
+    .el-icon-document {
+      font-size: 67px;
+      color: #c0c4cc;
+      margin: 40px 0 16px;
+      line-height: 50px;
+    }
+
+  }
+
+  .el-upload__text {
+    line-height: 30px;
+
+    .el-upload__tip {
+      margin-top: 0;
+    }
+  }
+
+  .el-upload {
+    width: 100%;
+
+    .el-upload-dragger {
+      width: 100%;
+    }
+  }
+</style>
