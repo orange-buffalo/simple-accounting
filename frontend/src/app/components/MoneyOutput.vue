0cd0d0732a6d1797d9ba51fee8d042fb5edb33a1
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-11-18 21:12:31 +1100

    reducing responsibility of money output

--- frontend/src/app/components/MoneyOutput.vue
+++ frontend/src/app/components/MoneyOutput.vue
@@ -1,83 +1,28 @@
 <template>
-  <div class="money-output">
-    <span>{{ defaultCurrencyAmountLabel }}</span>
-    <template v-if="amountConverted">
-      <br/>
-      <span class="original-amount">{{ originalCurrencyAmountLabel}}</span>
-    </template>
-  </div>
+  <span>{{ amountLabel }}</span>
 </template>
 
 <script>
-  import {mapState, mapGetters, mapActions} from 'vuex'
+  import {mapGetters} from 'vuex'
 
   export default {
     name: 'MoneyOutput',
 
     props: {
       currency: String,
-      originalAmount: Number,
-      amountInDefaultCurrency: Number,
-      actualAmountInDefaultCurrency: Number
-    },
-
-    data: function () {
-      return {}
-    },
-
-    created: function () {
-
-    },
-
-    methods: {
-      ...mapActions('app', {
-        loadCurrencies: 'loadCurrencies'
-      })
-
-
+      amount: Number
     },
 
     computed: {
-      ...mapState({
-        defaultCurrency: state => state.workspaces.currentWorkspace.defaultCurrency
-      }),
-
       ...mapGetters({
         getCurrencyFormatter: 'i18n/getCurrencyFormatter'
       }),
 
       //todo retrieve currency precision and convert from cents to currency units
 
-      defaultCurrencyAmountLabel: function () {
-        if (this.amountConverted) {
-          return this.getCurrencyFormatter(this.defaultCurrency)(this.amountInDefaultCurrency)
-        }
-        else {
-          return this.getCurrencyFormatter(this.defaultCurrency)(this.originalAmount)
-        }
-      },
-
-      originalCurrencyAmountLabel: function () {
-        return this.getCurrencyFormatter(this.currency)(this.originalAmount)
-      },
-
-      amountConverted: function () {
-        return this.currency !== this.defaultCurrency
+      amountLabel: function () {
+        return this.getCurrencyFormatter(this.currency)(this.amount)
       }
-
-
     }
   }
 </script>
-
-<style lang="scss">
-  .money-output {
-    text-align: right;
-
-    .original-amount {
-      font-size: 90%;
-      color: #606060;
-    }
-
-  }
-</style>
--- frontend/src/app/views/expenses/ExpensesOverview.vue
+++ frontend/src/app/views/expenses/ExpensesOverview.vue
@@ -11,18 +11,40 @@
           </template>
         </el-table-column>
 
-        <el-table-column label="currency">
-          <money-output slot-scope="scope"
-                        :currency="scope.row.currency"
-                        :original-amount="scope.row.originalAmount"
-                        :amount-in-default-currency="scope.row.amountInDefaultCurrency"
-                        :actual-amount-in-default-currency="scope.row.actualAmountInDefaultCurrency"/>
+        <el-table-column label="Amount"
+                         header-align="right">
+          <div class="money-output"
+               slot-scope="scope">
+            <money-output :currency="defaultCurrency"
+                          :amount="amountInDefaultCurrency(scope.row)"/>
+            <template v-if="isConverted(scope.row)">
+              <br/>
+              <money-output :currency="scope.row.currency"
+                            :amount="scope.row.originalAmount"
+                            class="secondary-text"/>
+            </template>
+          </div>
         </el-table-column>
 
-        <el-table-column
-            prop="percentOnBusinessInBps"
-            label="percentOnBusinessInBps">
+        <el-table-column label="Reported Amount"
+                         header-align="right">
+          <div class="money-output"
+               slot-scope="scope">
+            <!--todo reportedAmountInDefaultCurrency -->
+            <money-output :currency="defaultCurrency"
+                          :amount="scope.row.reportedAmountInDefaultCurrency"/>
+            <template v-if="scope.row.percentOnBusinessInBps < 10000">
+              <br/>
+              <!--todo format number as per locale -->
+              <div class="secondary-text">
+                <span>{{scope.row.percentOnBusinessInBps / 100}}% of </span>
+                <money-output :currency="defaultCurrency"
+                              :amount="scope.row.amountInDefaultCurrency"/>
+              </div>
+            </template>
+          </div>
         </el-table-column>
+
         <el-table-column
             prop="notes"
             label="notes">
@@ -40,22 +62,48 @@
 
   export default {
     name: 'ExpensesOverview',
+
     components: {
       DataTable,
       MoneyOutput
     },
+
     computed: {
       ...mapState({
-        workspaceId: state => state.workspaces.currentWorkspace.id
+        workspaceId: state => state.workspaces.currentWorkspace.id,
+        defaultCurrency: state => state.workspaces.currentWorkspace.defaultCurrency
       }),
+
       ...mapGetters({
         categoryById: 'workspaces/categoryById'
       })
     },
+
     methods: {
       navigateToCreateExpenseView: function () {
         this.$router.push({name: 'create-new-expense'})
+      },
+
+      amountInDefaultCurrency: function (expense) {
+        return expense.currency === this.defaultCurrency ? expense.originalAmount : expense.amountInDefaultCurrency
+      },
+
+      isConverted: function (expense) {
+        return expense.currency !== this.defaultCurrency
+            && expense.amountInDefaultCurrency
       }
     }
   }
 </script>
+
+<style lang="scss">
+  .money-output {
+    text-align: right;
+
+    .secondary-text {
+      font-size: 90%;
+      color: #8e8e8e;
+    }
+
+  }
+</style>
