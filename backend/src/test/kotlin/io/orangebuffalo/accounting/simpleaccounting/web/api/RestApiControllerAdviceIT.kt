11d91b20495dee0748f6226d1ce797b0d98bee13
orange-buffalo <orange-buffalo@users.noreply.github.com>
2018-11-23 10:58:19 +1100

    fixing rest api controller advice test

--- backend/src/test/kotlin/io/orangebuffalo/accounting/simpleaccounting/web/api/RestApiControllerAdviceIT.kt
+++ backend/src/test/kotlin/io/orangebuffalo/accounting/simpleaccounting/web/api/RestApiControllerAdviceIT.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
@@ -28,6 +30,12 @@ internal class RestApiControllerAdviceIT(
             .exchange()
             .expectStatus().isBadRequest
             .expectBody<String>().isEqualTo("Invalid request")
+    }
+
+    @TestConfiguration
+    class TestConfig {
+        @Bean
+        fun testRestController(): ApiControllerAdviceTestController = ApiControllerAdviceTestController()
     }
 
     @RestController
