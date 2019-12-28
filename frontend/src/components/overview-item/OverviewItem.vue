54fa84b47156d923b32d7c0428ebecc7f8e57560
orange-buffalo <orange-buffalo@users.noreply.github.com>
2019-12-29 08:18:57 +1100

    fixing overview item panel styling

--- frontend/src/components/overview-item/OverviewItem.vue
+++ frontend/src/components/overview-item/OverviewItem.vue
@@ -77,8 +77,6 @@
   /*todo #73: common component refers to app styles - redesign dependencies  */
   @import "~@/styles/vars.scss";
 
-  $trigger-button-size: 30px;
-
   .overview-item {
     &__panel {
       padding: 20px;
@@ -140,8 +138,7 @@
     &__details {
       border: 1px solid $secondary-grey;
       background-color: $white;
-      padding: 20px 20px 0 20px;
-      margin-top: -$trigger-button-size;
+      padding: 10px 20px 0 20px;
     }
   }
 </style>
