7c301adb32a70765e1cd5a155301b51fc9675884
orange-buffalo <orange-buffalo@users.noreply.github.com>
2020-05-23 12:07:55 +1000

    switching tests to the random port

--- backend/src/test/kotlin/io/orangebuffalo/simpleaccounting/junit/SimpleAccountingIntegrationTest.kt
+++ backend/src/test/kotlin/io/orangebuffalo/simpleaccounting/junit/SimpleAccountingIntegrationTest.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class, TestDataExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
