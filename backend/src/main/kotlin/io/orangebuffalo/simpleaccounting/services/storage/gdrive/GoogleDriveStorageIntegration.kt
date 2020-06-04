cbce2599a4674b2e738f8be15f4c5c3e567e5f44
orange-buffalo <orange-buffalo@users.noreply.github.com>
2020-06-05 08:01:00 +1000

    adding unique constraint for google drive integration entity #225

--- backend/src/main/kotlin/io/orangebuffalo/simpleaccounting/services/storage/gdrive/GoogleDriveStorageIntegration.kt
+++ backend/src/main/kotlin/io/orangebuffalo/simpleaccounting/services/storage/gdrive/GoogleDriveStorageIntegration.kt
@@ -3,7 +3,6 @@ package io.orangebuffalo.simpleaccounting.services.storage.gdrive
 import io.orangebuffalo.simpleaccounting.services.persistence.entities.AbstractEntity
 import org.springframework.data.relational.core.mapping.Table
 
-// todo #225: unique constraint
 @Table
 class GoogleDriveStorageIntegration(
     val userId: Long,
