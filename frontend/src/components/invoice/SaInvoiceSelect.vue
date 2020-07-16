7d8247a3b12afc1fe330244ceddc031ddbc73cda
orange-buffalo <orange-buffalo@users.noreply.github.com>
2020-07-17 08:16:45 +1000

    improving SaInvoiceSelect to display issue date and amount #14

--- frontend/src/components/invoice/SaInvoiceSelect.vue
+++ frontend/src/components/invoice/SaInvoiceSelect.vue
@@ -1,16 +1,34 @@
 <template>
   <SaEntitySelect
+    v-slot="{ entity: invoice }"
     v-bind="props"
     entity-path="invoices"
     :placeholder="$t('saInvoiceSelect.placeholder')"
     :label-provider="invoiceLabelProvider"
     class="sa-invoice-select"
     v-on="listeners"
-  />
+  >
+    <div class="sa-invoice-select__option">
+      <div class="sa-invoice-select__option__title">
+        {{ invoice.title }}
+      </div>
+      <div class="sa-invoice-select__option__date">
+        <SaIcon icon="calendar" />
+        {{ $t('common.date.medium', [invoice.dateIssued]) }}
+      </div>
+      <MoneyOutput
+        :amount="invoice.amount"
+        :currency="invoice.currency"
+        class="sa-invoice-select__option__amount"
+      />
+    </div>
+  </SaEntitySelect>
 </template>
 
 <script>
   import SaEntitySelect from '@/components/SaEntitySelect';
+  import SaIcon from '@/components/SaIcon';
+  import MoneyOutput from '@/components/MoneyOutput';
 
   function invoiceLabelProvider(invoice) {
     return invoice.title;
@@ -18,6 +36,8 @@
 
   export default {
     components: {
+      MoneyOutput,
+      SaIcon,
       SaEntitySelect,
     },
 
@@ -43,5 +63,29 @@
 
   .sa-invoice-select {
     min-width: 400px;
+
+    &__option {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      padding-bottom: 10px;
+
+      &__title {
+        width: 100%;
+      }
+
+      &__date, &__amount {
+        font-size: 90%;
+        line-height: 1em;
+        color: $secondary-color;
+      }
+
+      &__date {
+        .sa-icon {
+          width: 14px;
+          height: 14px;
+        }
+      }
+    }
   }
 </style>
