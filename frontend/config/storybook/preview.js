77f05d28d6b9a44f00484984912d7cf250d182e1
orange-buffalo <orange-buffalo@users.noreply.github.com>
2021-01-18 14:34:59 +1100

    adjusting storybook setup after upgrade

--- frontend/config/storybook/preview.js
+++ frontend/config/storybook/preview.js
@@ -5,6 +5,9 @@ import { createStoreMockDecorator } from '../../tests/storybook/utils/stories-st
 import { createApiMockDecorator } from '../../tests/storybook/utils/stories-api-mocks';
 import createRouterMockDecorator from '../../tests/storybook/utils/stories-router-mocks';
 
+// eslint-disable-next-line import/prefer-default-export
+export const parameters = { layout: 'fullscreen' };
+
 addDecorator(createStoreMockDecorator());
 addDecorator(createApiMockDecorator());
 addDecorator(createStoriesAppDecorator());
