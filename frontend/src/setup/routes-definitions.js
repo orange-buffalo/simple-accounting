4ae10946e4d4f837a42e2a4d082bf46c2277c8c7
orange-buffalo <orange-buffalo@users.noreply.github.com>
2021-04-28 08:01:15 +1000

    fixing navigation to dashboard after login closes #269

--- frontend/src/setup/routes-definitions.js
+++ frontend/src/setup/routes-definitions.js
@@ -68,10 +68,10 @@ export default new Router({
     {
       path: '/',
       component: TheAuthenticatedPage,
-      name: 'root',
       children: [
         {
           path: '',
+          name: 'dashboard',
           component: Dashboard,
         },
         {
@@ -217,11 +217,14 @@ export default new Router({
           name: 'create-new-user',
           component: CreateUser,
         },
-        {
-          path: '*',
-          redirect: '/',
-        },
       ],
+    },
+
+    {
+      path: '*',
+      redirect: {
+        name: 'dashboard',
+      },
     },
   ],
 });
