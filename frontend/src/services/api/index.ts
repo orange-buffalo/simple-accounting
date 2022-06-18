15179c8582512a6cbfbe44bd0e4d8907b5345eda
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-06-18 21:24:34 +1000

    #377: migrating use-last-view.ts, use-navigation.ts, assets

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,4 +1,4 @@
 export * from './api-types';
-export { workspacesApi } from './api-client';
+export { workspacesApi, profileApi } from './api-client';
 export { useAuth } from './auth';
 export * from './api-utils';
