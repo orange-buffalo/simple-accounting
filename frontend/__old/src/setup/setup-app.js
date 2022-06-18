3864f57714e1f04d5042dea6d7c880d189d25879
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-06-18 12:33:47 +1000

    #377: setting up l10n data generation

--- frontend/__old/src/setup/setup-app.js
+++ frontend/__old/src/setup/setup-app.js
@@ -1,32 +1,4 @@
 import Vue from 'vue';
-import {
-  Pagination,
-  Menu,
-  Input,
-  InputNumber,
-  Checkbox,
-  Switch,
-  Select,
-  Option,
-  OptionGroup,
-  Button,
-  Table,
-  TableColumn,
-  DatePicker,
-  Tooltip,
-  Form,
-  FormItem,
-  Progress,
-  Steps,
-  Step,
-  Container,
-  Header,
-  Aside,
-  Main,
-  Footer,
-  Loading,
-  Alert,
-} from 'element-ui';
 import ElementLocale from 'element-ui/lib/locale';
 import VueCompositionApi from '@vue/composition-api';
 import SimpleAccounting from '@/SimpleAccounting.vue';
@@ -37,33 +9,6 @@ import setupErrorHandler from '@/setup/setup-error-handler';
 import { app } from '@/services/app-services';
 
 function setupElementUi({ i18n }) {
-  Vue.use(Pagination);
-  Vue.use(Menu);
-  Vue.use(Input);
-  Vue.use(InputNumber);
-  Vue.use(Checkbox);
-  Vue.use(Switch);
-  Vue.use(Select);
-  Vue.use(Option);
-  Vue.use(OptionGroup);
-  Vue.use(Button);
-  Vue.use(Table);
-  Vue.use(TableColumn);
-  Vue.use(DatePicker);
-  Vue.use(Tooltip);
-  Vue.use(Form);
-  Vue.use(FormItem);
-  Vue.use(Progress);
-  Vue.use(Steps);
-  Vue.use(Step);
-  Vue.use(Container);
-  Vue.use(Header);
-  Vue.use(Aside);
-  Vue.use(Main);
-  Vue.use(Footer);
-  Vue.use(Loading.directive);
-  Vue.use(Alert);
-
   ElementLocale.i18n((key, value) => i18n.t(key, value));
 }
 
