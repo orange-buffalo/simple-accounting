b9f70ef3a1965b4a9131356f5ae46cfc84768f60
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-07-02 16:47:12 +1000

    #377: migrating SaIcon to setup script

--- frontend/src/components/SaIcon.vue
+++ frontend/src/components/SaIcon.vue
@@ -1,45 +1,34 @@
 <template>
-  <span class="sa-icon" v-html="loadedIcon" @click="$emit('click')"/>
+  <span class="sa-icon" v-html="loadedIcon" @click="$emit('click')" />
 </template>
 
-<script lang="ts">
-  import { defineComponent, shallowRef, watch } from 'vue';
+<script lang="ts" setup>
+  import { shallowRef, watch } from 'vue';
   import { iconByName } from '@/icons';
 
-  export default defineComponent({
-    props: {
-      icon: {
-        type: String,
-        required: true,
-      },
-    },
+  const props = defineProps<{
+    icon: string;
+  }>();
 
-    setup(props) {
-      const loadedIcon = shallowRef<string | null>(null);
+  const loadedIcon = shallowRef<string | null>(null);
 
-      watch(() => props.icon, async (icon) => {
-        loadedIcon.value = iconByName(icon);
-      }, { immediate: true });
-
-      return {
-        loadedIcon,
-      };
-    },
-  });
+  watch(() => props.icon, async (icon) => {
+    loadedIcon.value = iconByName(icon);
+  }, { immediate: true });
 </script>
 
 <style lang="scss">
-.sa-icon {
-  display: inline-flex;
-  width: 16px;
-  height: 16px;
+  .sa-icon {
+    display: inline-flex;
+    width: 16px;
+    height: 16px;
 
-  svg {
-    width: 100%;
-    height: 100%;
-    color: inherit;
-    fill: currentColor;
-    stroke: none;
+    svg {
+      width: 100%;
+      height: 100%;
+      color: inherit;
+      fill: currentColor;
+      stroke: none;
+    }
   }
-}
 </style>
