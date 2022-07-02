94949502ede4210b2a0d2324f25b57cc8857c69f
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-07-02 20:16:13 +1000

    #377: implementing tests for api

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,4 +1,6 @@
 export * from './api-types';
 export { workspacesApi, profileApi } from './api-client';
 export { useAuth } from './auth';
+export type { Auth } from './auth';
 export * from './api-utils';
+export type { ProfileApiControllerApi, ResponseError } from './generated';
