859d423c5dc0ce19deb1b836e77c5dd6781ee625
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-07-02 18:48:54 +1000

    #377: migrating login view

--- frontend/src/SimpleAccounting.vue
+++ frontend/src/SimpleAccounting.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    simple accounting body
-    <ElButton type="primary">Primary</ElButton>
-    <ElTooltip
-        class="box-item"
-        effect="dark"
-        content="Top Left prompts info"
-        placement="top-start"
-      >
-        <ElButton>top-start</ElButton>
-      </ElTooltip>
-<!--    <RouterView />-->
+    <RouterView />
   </div>
 </template>
