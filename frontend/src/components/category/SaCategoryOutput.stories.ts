af9c54a2a394c73d6102d8b76ee2089adb1fbd38
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-07-31 10:46:33 +1000

    #377, #126: migrating SaCategoryOutput

---
+++ frontend/src/components/category/SaCategoryOutput.stories.ts
@@ -0,0 +1,35 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { fetchMock, defaultWorkspacePath, neverEndingGetRequest } from '@/__storybook__/api-mocks';
+import SaCategoryOutput from '@/components/category/SaCategoryOutput.vue';
+import { defineStory } from '@/__storybook__/sa-storybook';
+import { disableOutputLoaderAnimations, waitForText } from '@/__storybook__/screenshots';
+
+export default {
+  title: 'Components/SaCategoryOutput',
+};
+
+export const Loaded = defineStory(() => ({
+  components: { SaCategoryOutput },
+  template: '<SaCategoryOutput :category-id="42"/>',
+  beforeCreate() {
+    fetchMock.get(defaultWorkspacePath('/categories'), {
+      data: [{
+        id: 42,
+        name: 'Category 42',
+      }],
+    });
+  },
+}), {
+  screenshotPreparation: waitForText('Category 42'),
+});
+
+export const Loading = defineStory(() => ({
+  components: { SaCategoryOutput },
+  template: '<SaCategoryOutput :category-id="42"/>',
+  beforeCreate() {
+    fetchMock.get(defaultWorkspacePath('/categories'), {}, neverEndingGetRequest);
+  },
+}), {
+  screenshotPreparation: disableOutputLoaderAnimations(),
+});
