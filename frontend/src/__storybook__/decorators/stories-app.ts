66bfd1e834ca9d2b0059318f0023c092ad71d3e8
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-08-03 07:32:27 +1000

    #377: setting date mocks for storybook, clearing local storage for stories

--- frontend/src/__storybook__/decorators/stories-app.ts
+++ frontend/src/__storybook__/decorators/stories-app.ts
@@ -1,9 +1,6 @@
-// import { advanceTo, clear } from 'jest-date-mock';
 import { app as storybookApp } from '@storybook/vue3';
 import { router, setupApp } from '@/setup/setup-app';
 import StoriesApp from './StoriesApp.vue';
-// init client
-import '@/services/api';
 import { decoratorFactory } from '@/__storybook__/decorators/decorator-utils';
 
 // todo: probably mock router instead of setup?
@@ -13,12 +10,6 @@ storybookApp.use(router());
 export const createStoriesAppDecorator = decoratorFactory((parameters) => ({
   components: { StoriesApp },
   data() {
-    // if (parameters.skipMockTime) {
-    //   clear();
-    // } else {
-    //   advanceTo(new Date('2030-01-04T00:00:00'));
-    // }
-
     return {
       fullScreen: parameters.fullScreen,
     };
