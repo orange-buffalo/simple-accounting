d1d49be0d797140921980d0029f787b78f1a1727
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-09-05 07:23:13 +1000

    #377, #126: migrating NotesInput

--- frontend/__old/tests/storybook/utils/stories-utils.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from 'puppeteer';
-
-export async function pauseAndResetInputLoaderAnimation(page: Page) {
-  await pauseAndResetAnimation(page, '.sa-input-loader__indicator');
-}
-
-
