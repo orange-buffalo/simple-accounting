c2c9fdbd77b0d2d85ce54b0f90198a68107562e1
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-09-06 07:24:29 +1000

    #377, #126: migrating SaBasicErrorMessage.vue

---
+++ frontend/src/components/SaBasicErrorMessage.vue
@@ -0,0 +1,34 @@
+<template>
+  <SaStatusLabel
+    simplified
+    status="failure"
+    class="sa-basic-error-message"
+  >
+    {{ errorMessage }}
+  </SaStatusLabel>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  import { $t } from '@/services/i18n';
+  import SaStatusLabel from '@/components/SaStatusLabel.vue';
+
+  const props = defineProps<{ message?: string }>();
+
+  const errorMessage = computed(() => props.message || $t.value.saBasicErrorMessage.defaultMessage());
+</script>
+
+<style lang="scss">
+  @use "@/styles/vars.scss" as *;
+  @use "@/styles/mixins.scss"as *;
+
+  .sa-basic-error-message {
+    white-space: nowrap;
+
+    .sa-status-label__icon {
+      min-width: 18px;
+      min-height: 18px;
+      margin-right: 5px;
+    }
+  }
+</style>
