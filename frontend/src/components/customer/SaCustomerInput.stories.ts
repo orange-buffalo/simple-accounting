db0dec4b0ef30230cf8dec6ceaf23024d4dae991
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-09-07 07:18:28 +1000

    #377, #126: migrating SaCustomerInput

---
+++ frontend/src/components/customer/SaCustomerInput.stories.ts
@@ -0,0 +1,56 @@
+// noinspection JSUnusedGlobalSymbols
+
+import {
+  neverEndingGetRequest,
+  onGetToDefaultWorkspacePath,
+} from '@/__storybook__/api-mocks';
+import SaCustomerInput from '@/components/customer/SaCustomerInput.vue';
+import { defineStory } from '@/__storybook__/sa-storybook';
+import {
+  allOf, clickOnElement, waitForElementToBeVisible,
+} from '@/__storybook__/screenshots';
+import { storybookData } from '@/__storybook__/storybook-data';
+
+export default {
+  title: 'Components/SaCustomerInput',
+};
+
+export const Default = defineStory(() => ({
+  components: { SaCustomerInput },
+  data: () => ({
+    presetCustomerId: storybookData.customers.democraticOrderOfPlanets.id,
+    initiallyEmptyCustomerId: undefined,
+  }),
+  template: `
+    <h4>Empty value</h4>
+    <SaCustomerInput v-model="initiallyEmptyCustomerId"
+                     placeholder="Please select customer"
+                     clearable
+                     id="initially-empty-select"
+    />
+    <br />
+    {{ initiallyEmptyCustomerId }}
+
+    <h4>Preset value</h4>
+    <SaCustomerInput v-model="presetCustomerId"
+                     id="preset-select"
+    />
+    <br />
+    {{ presetCustomerId }}
+  `,
+  ...storybookData.storyComponentConfig,
+}), {
+  screenshotPreparation: allOf(
+    waitForElementToBeVisible('#preset-select > .el-select'),
+    clickOnElement('#initially-empty-select > .el-select'),
+    waitForElementToBeVisible('.el-select-dropdown'),
+  ),
+});
+
+export const Loading = defineStory(() => ({
+  components: { SaCustomerInput },
+  template: '<SaCustomerInput />',
+  beforeCreate() {
+    onGetToDefaultWorkspacePath('/customers', {}, neverEndingGetRequest);
+  },
+}));
---
+++ frontend/src/components/customer/SaCustomerInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <SaInputLoader :loading="loading">
+    <ElSelect
+      :model-value="modelValue"
+      @update:modelValue="emit('update:modelValue', $event || undefined)"
+      :placeholder="placeholder"
+      :clearable="clearable"
+    >
+      <ElOption
+        v-for="customer in customers"
+        :key="customer.id"
+        :label="customer.name"
+        :value="customer.id"
+      />
+    </ElSelect>
+  </SaInputLoader>
+</template>
+
+<script lang="ts" setup>
+  import SaInputLoader from '@/components/SaInputLoader.vue';
+  import { useValueLoadedByCurrentWorkspace } from '@/services/utils';
+  import { consumeAllPages, customerApi } from '@/services/api';
+
+  defineProps<{
+    modelValue?: number,
+    placeholder?: string,
+    clearable?: boolean,
+  }>();
+
+  const emit = defineEmits<{(e: 'update:modelValue', value: number): void }>();
+
+  const {
+    value: customers,
+    loading,
+  } = useValueLoadedByCurrentWorkspace((workspaceId) => consumeAllPages((pageRequest) => customerApi.getCustomers({
+    ...pageRequest,
+    workspaceId,
+  })));
+</script>
