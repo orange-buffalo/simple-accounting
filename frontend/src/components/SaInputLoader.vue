b97e600307f0d3a97540ac7a3a336a20f1820078
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-09-07 06:43:58 +1000

    #377, #126: migrating SaInputLoader

---
+++ frontend/src/components/SaInputLoader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sa-input-loader">
+    <div
+      v-if="loading"
+      class="sa-input-loader__indicator"
+    />
+
+    <SaBasicErrorMessage v-else-if="error" />
+
+    <slot v-else />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import SaBasicErrorMessage from '@/components/SaBasicErrorMessage.vue';
+
+  defineProps<{
+    loading: boolean,
+    error?: boolean,
+  }>();
+</script>
+
+<style lang="scss">
+  @use "@/styles/vars.scss" as *;
+  @use "@/styles/mixins.scss" as *;
+
+  .sa-input-loader {
+    &__indicator {
+      height: $sa-input-height;
+      @include loading-placeholder;
+      @include input-width;
+    }
+  }
+</style>
