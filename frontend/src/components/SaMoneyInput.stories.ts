00d2cdfe3731479ac14177de50aa7b8332043063
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-09-14 22:19:59 +1000

    #377: improving screenshot tests

--- frontend/src/components/SaMoneyInput.stories.ts
+++ frontend/src/components/SaMoneyInput.stories.ts
@@ -1,28 +1,32 @@
 // noinspection JSUnusedGlobalSymbols
 
 import SaMoneyInput from '@/components/SaMoneyInput.vue';
+import { defineStory } from '@/__storybook__/sa-storybook';
+import { waitForText } from '@/__storybook__/screenshots';
 
 export default {
   title: 'Components/SaMoneyInput',
 };
 
-export const Default = () => ({
+export const Default = defineStory(() => ({
   components: { SaMoneyInput },
   data: () => ({
-      audValue: 783,
-      yenValue: undefined,
-      kwdValue: 0,
-    }),
+    audValue: 783,
+    yenValue: undefined,
+    kwdValue: 0,
+  }),
   template: `
     <h4>With initial value</h4>
     <SaMoneyInput v-model="audValue" currency="AUD" />
-    <br/>{{audValue}} 
-    <br/>
+    <br />{{audValue}}
+    <br />
     <h4>No initial value</h4>
     <SaMoneyInput v-model="yenValue" currency="JPY" />
-    <br/>{{yenValue}}
+    <br />{{yenValue}}
     <h4>Currency 3 digits</h4>
     <SaMoneyInput v-model="kwdValue" currency="KWD" />
-    <br/>{{kwdValue}}
+    <br /><span id="kwdValue">{{ kwdValue }}</span>
   `,
+}), {
+  screenshotPreparation: waitForText('0', '#kwdValue'),
 });
