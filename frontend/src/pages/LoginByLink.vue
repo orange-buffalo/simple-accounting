e9d7de74778d04b0315bfd5aa801521931fcc3d5
orange-buffalo <orange-buffalo@users.noreply.github.com>
2022-10-30 08:58:45 +1100

    #377, #126: migrating LoginByLink

---
+++ frontend/src/pages/LoginByLink.vue
@@ -0,0 +1,53 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts" setup>
+  import { onMounted } from 'vue';
+  import { initWorkspace, useWorkspaces } from '@/services/workspaces';
+  import useNavigation from '@/services/use-navigation';
+  import { useAuth, workspacesApi } from '@/services/api';
+
+  const props = defineProps<{
+    token?: string,
+  }>();
+
+  const { navigateByPath } = useNavigation();
+
+  onMounted(async () => {
+    if (props.token === undefined) {
+      await navigateByPath('/');
+      return;
+    }
+    const {
+      isLoggedIn,
+      loginBySharedToken,
+    } = useAuth();
+
+    try {
+      if (isLoggedIn()) {
+        const workspace = await workspacesApi.saveSharedWorkspace({
+          saveSharedWorkspaceRequestDto: {
+            token: props.token,
+          },
+        });
+
+        const {
+          loadWorkspaces,
+          setCurrentWorkspace,
+        } = useWorkspaces();
+        setCurrentWorkspace(workspace);
+        await loadWorkspaces();
+        await navigateByPath('/');
+      } else if (await loginBySharedToken(props.token)) {
+        await initWorkspace();
+        await navigateByPath('/');
+      } else {
+        // todo #117 set login error and update ui accordingly
+      }
+    } catch (e) {
+      // todo #117: handle communication exception and update ui accordingly
+      console.log('error', e);
+    }
+  });
+</script>
