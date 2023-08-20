882c4b500e6465d1481485e66c1216bba02cda58
orange-buffalo <orange-buffalo@users.noreply.github.com>
2023-08-20 13:02:33 +1000

    chore: disabling false-positive eslint rule

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -54,6 +54,7 @@ module.exports = {
     'vue/multi-word-component-names': 'off',
     'vue/require-default-prop': 'off',
     'vuejs-accessibility/form-control-has-label': 'off',
+    'vue/no-setup-props-destructure': 'off',
   },
   globals: {
     RequestInit: 'readonly',
