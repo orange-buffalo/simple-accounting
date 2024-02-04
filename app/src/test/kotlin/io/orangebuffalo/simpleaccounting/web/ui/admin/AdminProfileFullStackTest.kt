0417efa8a2452c9d4af8338acf218200b84e3768
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-02-04 15:16:13 +1100

    tests: Covering My Profile page sections visibility with tests Related to #100

---
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/admin/AdminProfileFullStackTest.kt
@@ -0,0 +1,30 @@
+package io.orangebuffalo.simpleaccounting.web.ui.admin
+
+import com.microsoft.playwright.Page
+import io.orangebuffalo.simpleaccounting.infra.SimpleAccountingFullStackTest
+import io.orangebuffalo.simpleaccounting.infra.database.Prototypes
+import io.orangebuffalo.simpleaccounting.infra.database.TestData
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.loginAs
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.shouldBeMyProfilePage
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.shouldHaveSideMenu
+import org.junit.jupiter.api.Test
+
+@SimpleAccountingFullStackTest
+class AdminProfileFullStackTest {
+
+    @Test
+    fun `should render My Profile page with proper sections`(testData: AdminProfileTestData, page: Page) {
+        page.loginAs(testData.admin)
+
+        page.shouldHaveSideMenu().clickMyProfile()
+
+        page.shouldBeMyProfilePage()
+            .shouldHaveDocumentsStorageSectionHidden()
+            .shouldHaveLanguagePreferencesSectionVisible()
+            .shouldHavePasswordChangeSectionVisible()
+    }
+
+    class AdminProfileTestData : TestData {
+        val admin = Prototypes.farnsworth()
+    }
+}
--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/shared/pages/MyProfilePage.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/shared/pages/MyProfilePage.kt
@@ -2,15 +2,80 @@ package io.orangebuffalo.simpleaccounting.web.ui.shared.pages
 
 import com.microsoft.playwright.Page
 import io.orangebuffalo.simpleaccounting.infra.ui.components.*
+import io.orangebuffalo.simpleaccounting.infra.utils.assertThat
 
 class MyProfilePage(page: Page) : SaPageBase<MyProfilePage>(page) {
     private val header = components.pageHeader("My Profile")
-    val currentPassword = components.formItemByLabel("Current Password", TextInput::class)
-    val newPassword = components.formItemByLabel("New Password", TextInput::class)
-    val newPasswordConfirmation = components.formItemByLabel("New Password Confirmation", TextInput::class)
-    val changePasswordButton = components.buttonByText("Apply new password")
+    private val passwordChangeForm = PasswordChangeForm(page, components)
+    private val documentsStorageSection = DocumentStorageSection(page)
+    private val languagePreferencesSection = LanguagePreferencesSection(page)
 
     fun shouldBeOpen(): MyProfilePage = header.shouldBeVisible()
+
+    fun shouldHavePasswordChangeSectionVisible(): MyProfilePage {
+        passwordChangeForm.shouldBeVisible()
+        return this
+    }
+
+    fun shouldHaveLanguagePreferencesSectionVisible(): MyProfilePage {
+        languagePreferencesSection.shouldBeVisible()
+        return this
+    }
+
+    fun shouldHavePasswordChangeSectionVisible(spec: PasswordChangeForm.() -> Unit): MyProfilePage {
+        passwordChangeForm.shouldBeVisible()
+        passwordChangeForm.spec()
+        return this
+    }
+
+    fun shouldHaveDocumentsStorageSectionVisible(): MyProfilePage {
+        documentsStorageSection.shouldBeVisible()
+        return this
+    }
+
+    fun shouldHaveDocumentsStorageSectionHidden(): MyProfilePage {
+        documentsStorageSection.shouldBeHidden()
+        return this
+    }
+
+    class PasswordChangeForm(page: Page, components: ComponentsAccessors<MyProfilePage>) {
+        val currentPassword = components.formItemByLabel("Current Password", TextInput::class)
+        val newPassword = components.formItemByLabel("New Password", TextInput::class)
+        val newPasswordConfirmation = components.formItemByLabel("New Password Confirmation", TextInput::class)
+        val changePasswordButton = components.buttonByText("Apply new password")
+        private val passwordChangeSectionHeader =
+            page.locator("//*[contains(@class, 'el-form')]//h2[text()='Change Password']")
+
+        fun shouldBeVisible() {
+            passwordChangeSectionHeader.assertThat().isVisible()
+        }
+    }
+
+    class DocumentStorageSection(page: Page) {
+        private val documentStorageSectionHeader =
+            page.locator("//*[contains(@class, 'el-form')]//h2[text()='Documents Storage']")
+
+        fun shouldBeVisible() {
+            documentStorageSectionHeader.assertThat().isVisible()
+        }
+
+        fun shouldBeHidden() {
+            documentStorageSectionHeader.assertThat().isHidden()
+        }
+    }
+
+    class LanguagePreferencesSection(page: Page) {
+        private val languagePreferencesSectionHeader =
+            page.locator("//*[contains(@class, 'el-form')]//h2[text()='Language Preferences']")
+
+        fun shouldBeVisible() {
+            languagePreferencesSectionHeader.assertThat().isVisible()
+        }
+
+        fun shouldBeHidden() {
+            languagePreferencesSectionHeader.assertThat().isHidden()
+        }
+    }
 }
 
 fun Page.shouldBeMyProfilePage(): MyProfilePage = MyProfilePage(this).shouldBeOpen()
