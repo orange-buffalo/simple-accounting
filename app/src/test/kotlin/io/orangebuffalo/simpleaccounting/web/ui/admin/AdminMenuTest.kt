37efa053d99aa51e1fbc0aab30ce39a3fca14205
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-02-04 14:34:51 +1100

    tests: Covering admin menu with test Related to #100

---
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/admin/AdminMenuTest.kt
@@ -0,0 +1,30 @@
+package io.orangebuffalo.simpleaccounting.web.ui.admin
+
+import com.microsoft.playwright.Page
+import io.orangebuffalo.simpleaccounting.infra.SimpleAccountingFullStackTest
+import io.orangebuffalo.simpleaccounting.infra.database.Prototypes
+import io.orangebuffalo.simpleaccounting.infra.database.TestData
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.NavigationMenu
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.loginAs
+import io.orangebuffalo.simpleaccounting.web.ui.shared.pages.shouldHaveSideMenu
+import org.junit.jupiter.api.Test
+
+@SimpleAccountingFullStackTest
+class AdminMenuTest {
+
+    @Test
+    fun `should render proper menu to admin user`(testData: AdminMenuTestData, page: Page) {
+        page.loginAs(testData.admin)
+        page.shouldHaveSideMenu()
+            .shouldHaveItems(
+                NavigationMenu.MenuItem("Users", false),
+                NavigationMenu.MenuItem("USER", true),
+                NavigationMenu.MenuItem("My Profile", false),
+                NavigationMenu.MenuItem("Logout", false),
+            )
+    }
+
+    class AdminMenuTestData : TestData {
+        val admin = Prototypes.farnsworth()
+    }
+}
--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/shared/pages/NavigationMenu.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/web/ui/shared/pages/NavigationMenu.kt
@@ -1,7 +1,10 @@
 package io.orangebuffalo.simpleaccounting.web.ui.shared.pages
 
 import com.microsoft.playwright.Page
+import io.kotest.matchers.collections.shouldContainInOrder
 import io.orangebuffalo.simpleaccounting.infra.utils.assertThat
+import io.orangebuffalo.simpleaccounting.infra.utils.hasClass
+import io.orangebuffalo.simpleaccounting.infra.utils.innerTextOrNull
 
 class NavigationMenu(private val page: Page) {
     private val container = page.locator(".side-menu")
@@ -14,6 +17,19 @@ class NavigationMenu(private val page: Page) {
         page.getByText("My Profile").click()
         return this
     }
+
+    fun shouldHaveItems(vararg expectedItems: MenuItem) {
+        val menuItems = page.locator(".side-menu__link, .side-menu__category")
+            .elementHandles()
+            .map { element ->
+                val label = element.innerTextOrNull()
+                val isSectionHeader = element.hasClass("side-menu__category")
+                MenuItem(label, isSectionHeader)
+            }
+        menuItems.shouldContainInOrder(*expectedItems)
+    }
+
+    data class MenuItem(val label: String?, val isSectionHeader: Boolean)
 }
 
 fun Page.shouldHaveSideMenu(): NavigationMenu = NavigationMenu(this).shouldBeVisible()
