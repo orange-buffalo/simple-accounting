dc00ed38645afc64cb168a5b3021a60e64e599ee
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-02-25 09:09:53 +1100

    tests: Hiding components low-level constructors

--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/infra/ui/components/FormItem.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/infra/ui/components/FormItem.kt
@@ -2,17 +2,16 @@ package io.orangebuffalo.simpleaccounting.infra.ui.components
 
 import com.microsoft.playwright.Locator
 import io.orangebuffalo.simpleaccounting.infra.utils.assertThat
-import kotlin.reflect.KClass
 
-class FormItem<I : Any, T : Any>(
+class FormItem<I : Any, T : Any> private constructor(
     rootLocator: Locator,
     private val parent: T,
-    inputClass: KClass<I>
+    inputProvider: (container: Locator) -> I
 ) {
     private val validationErrorLocator = rootLocator.locator(".el-form-item__error")
     private val inputLocator =
         rootLocator.locator("xpath=//*[@class='el-form-item__content']/*[@class != 'el-form-item__error']")
-    private val input: I = inputClass.constructors.first().call(inputLocator, Unit)
+    private val input: I = inputProvider(inputLocator)
 
     operator fun invoke(action: I.(formItem: FormItem<I, *>) -> Unit): T {
         input.action(this)
@@ -24,7 +23,18 @@ class FormItem<I : Any, T : Any>(
         validationErrorLocator.assertThat().hasText(message)
         return parent
     }
-}
 
-fun <T : SaPageBase<T>, I : Any> ComponentsAccessors<T>.formItemByLabel(label: String, inputClass: KClass<I>) =
-    FormItem(page.locator("//*[@class='el-form-item__label' and text()='$label']/.."), this.owner, inputClass)
+    companion object {
+        private fun <T : SaPageBase<T>, I : Any> ComponentsAccessors<T>.formItemByLabel(
+            label: String,
+            inputProvider: (container: Locator) -> I
+        ) = FormItem(
+            rootLocator = page.locator("//*[@class='el-form-item__label' and text()='$label']/.."),
+            parent = this.owner,
+            inputProvider = inputProvider
+        )
+
+        fun <T : SaPageBase<T>> ComponentsAccessors<T>.formItemTextInputByLabel(label: String) =
+            formItemByLabel(label) { TextInput.byContainer(it) }
+    }
+}
