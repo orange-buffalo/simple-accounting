7fdbb36a83e0c732588a7a022c40e54f34b3aa08
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-05-05 20:12:53 +1000

    chore: handling new API errors in global error handler

--- frontend/src/setup/ErrorHandler.stories.ts
+++ frontend/src/setup/ErrorHandler.stories.ts
@@ -1,9 +1,63 @@
+import { onMounted, onUnmounted } from 'vue';
+import { profileApi, useCancellableRequest } from '@/services/api';
+import { fetchMock } from '@/__storybook__/api-mocks';
+
 export default {
   title: 'Components/Other/ErrorHandler',
 };
 
-export const Default = () => ({
+export const FatalApiError = () => ({
+  setup() {
+    onMounted(async () => {
+      fetchMock.get('api/profile', { status: 500 });
+      await profileApi.getProfile();
+    });
+
+    onUnmounted(() => {
+      fetchMock.reset();
+    });
+  },
   template: `
-    Erroneous expression: {{ undefinedVariable.execute() }}
+    <h3>Fatal error from API</h3>
+  `,
+});
+
+export const CancelledRequest = () => ({
+  setup() {
+    const {
+      cancellableRequestConfig,
+      cancelRequest,
+    } = useCancellableRequest();
+    onMounted(async () => {
+      cancelRequest();
+      fetchMock.get('api/profile', { delay: 20000 });
+      await profileApi.getProfile(cancellableRequestConfig);
+    });
+
+    onUnmounted(() => {
+      fetchMock.reset();
+    });
+  },
+  template: `
+    <h3>Cancelled API request</h3>
+  `,
+});
+
+export const ApiAuthError = () => ({
+  setup() {
+    onMounted(async () => {
+      fetchMock.get('api/profile', { status: 401 });
+      fetchMock.post('/api/auth/token', {
+      status: 401,
+    });
+      await profileApi.getProfile();
+    });
+
+    onUnmounted(() => {
+      fetchMock.reset();
+    });
+  },
+  template: `
+    <h3>API Auth error</h3>
   `,
 });
