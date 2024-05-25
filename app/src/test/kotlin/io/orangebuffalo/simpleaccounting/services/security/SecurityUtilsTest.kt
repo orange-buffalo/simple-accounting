5748047d2af8ef6172a56f9c1d4dead88419378e
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-05-25 10:59:21 +1000

    tests: Migrating SecurityUtilsTest from deprecated Prototypes

--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/services/security/SecurityUtilsTest.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/services/security/SecurityUtilsTest.kt
@@ -3,7 +3,8 @@ package io.orangebuffalo.simpleaccounting.services.security
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isTrue
-import io.orangebuffalo.simpleaccounting.infra.database.Prototypes
+import io.orangebuffalo.simpleaccounting.domain.users.I18nSettings
+import io.orangebuffalo.simpleaccounting.domain.users.PlatformUser
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 
@@ -12,7 +13,7 @@ class SecurityUtilsTest {
     @Test
     fun `should propagate security principal via runAs`() {
         val result = runBlocking {
-            runAs(Prototypes.fry().toSecurityPrincipal()) {
+            runAs(fry().toSecurityPrincipal()) {
                 assertThat(getCurrentPrincipal().userName).isEqualTo("Fry")
                 assertThat(getAuthentication().isAuthenticated).isTrue()
                 "result"
@@ -21,4 +22,11 @@ class SecurityUtilsTest {
         assertThat(result).isEqualTo("result")
     }
 
+    private fun fry() = PlatformUser(
+        userName = "Fry",
+        isAdmin = false,
+        passwordHash = "hash",
+        activated = true,
+        i18nSettings = I18nSettings(locale = "en", language = "en")
+    )
 }
