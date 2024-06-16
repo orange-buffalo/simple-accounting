cfe249fa41a9f99ec2262b6923e0e977b92de508
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-06-16 18:38:56 +1000

    tests: Refactoring UsersApiControllerIT as a preparation for further extension Relates to #1289

--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/domain/users/UsersApiControllerIT.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/domain/users/UsersApiControllerIT.kt
@@ -1,29 +1,29 @@
 package io.orangebuffalo.simpleaccounting.domain.users
 
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
-import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
 import io.orangebuffalo.simpleaccounting.infra.SimpleAccountingIntegrationTest
 import io.orangebuffalo.simpleaccounting.infra.api.sendJson
-import io.orangebuffalo.simpleaccounting.infra.api.verifyOkAndJsonBody
+import io.orangebuffalo.simpleaccounting.infra.api.verifyOkAndJsonBodyEqualTo
 import io.orangebuffalo.simpleaccounting.infra.database.Preconditions
 import io.orangebuffalo.simpleaccounting.infra.database.PreconditionsInfra
 import io.orangebuffalo.simpleaccounting.infra.security.WithMockFarnsworthUser
 import io.orangebuffalo.simpleaccounting.infra.security.WithMockFryUser
-import io.orangebuffalo.simpleaccounting.infra.utils.MOCK_TIME
-import io.orangebuffalo.simpleaccounting.infra.utils.mockCurrentTime
+import io.orangebuffalo.simpleaccounting.infra.utils.*
 import io.orangebuffalo.simpleaccounting.services.business.TimeService
-import net.javacrumbs.jsonunit.assertj.JsonAssertions
+import kotlinx.serialization.json.addJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonArray
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate
 import org.springframework.test.web.reactive.server.WebTestClient
+import java.time.Instant
 
 @SimpleAccountingIntegrationTest
-@DisplayName("Admin User API ")
+@DisplayName("Users API")
 internal class UsersApiControllerIT(
     @Autowired private val client: WebTestClient,
     @Autowired private val aggregateTemplate: JdbcAggregateTemplate,
@@ -31,119 +31,138 @@ internal class UsersApiControllerIT(
     @Autowired private val preconditionsInfra: PreconditionsInfra,
 ) {
 
-    @Test
-    @WithMockFryUser
-    fun `should allow access only for Admin to read users`() {
-        client.get()
-            .uri("/api/users")
-            .exchange()
-            .expectStatus().isForbidden
-    }
+    @Nested
+    @DisplayName("GET /api/users")
+    inner class GetUsers {
 
-    @Test
-    @WithMockFryUser
-    fun `should allow access only for Admin to create users`() {
-        client.post()
-            .uri("/api/users")
-            .exchange()
-            .expectStatus().isForbidden
-    }
+        private fun sendRequest() = client.get().uri("/api/users")
 
-    @Test
-    @WithMockFarnsworthUser
-    fun `should return a valid users page`() {
-        val testData = setupPreconditions()
-        client.get()
-            .uri("/api/users")
-            .verifyOkAndJsonBody {
-                inPath("$.pageNumber").isNumber.isEqualTo("1")
-                inPath("$.pageSize").isNumber.isEqualTo("10")
-                inPath("$.totalElements").isNumber.isEqualTo("3")
+        @Test
+        fun `should prohibit anonymous access`() {
+            sendRequest()
+                .exchange()
+                .expectStatus().isUnauthorized
+        }
 
-                inPath("$.data").isArray.containsExactly(
-                    JsonAssertions.json(
-                        """{
-                        userName: "Farnsworth",
-                        id: ${testData.farnsworth.id},
-                        version: 0,
-                        admin: true,
-                        activated: true
-                    }"""
-                    ),
-                    JsonAssertions.json(
-                        """{
-                        userName: "Fry",
-                        id: ${testData.fry.id},
-                        version: 0,
-                        admin: false,
-                        activated: true
-                    }"""
-                    ),
-                    JsonAssertions.json(
-                        """{
-                        userName: "Zoidberg",
-                        id: ${testData.zoidberg.id},
-                        version: 0,
-                        admin: false,
-                        activated: false
-                    }"""
-                    )
-                )
-            }
-    }
+        @Test
+        @WithMockFryUser
+        fun `should prohibit access by regular users`() {
+            sendRequest()
+                .exchange()
+                .expectStatus().isForbidden
+        }
 
-    @Test
-    @WithMockFarnsworthUser
-    fun `should create a new user`() {
-        setupPreconditions()
-        mockCurrentTime(timeService)
+        @Test
+        @WithMockFarnsworthUser
+        fun `should return a valid users page`() {
+            val testData = setupPreconditions()
+            sendRequest()
+                .verifyOkAndJsonBodyEqualTo {
+                    put("pageNumber", 1)
+                    put("pageSize", 10)
+                    put("totalElements", 3)
+                    putJsonArray("data") {
+                        addJsonObject {
+                            put("userName", "Farnsworth")
+                            put("id", testData.farnsworth.id)
+                            put("version", 0)
+                            put("admin", true)
+                            put("activated", true)
+                        }
+                        addJsonObject {
+                            put("userName", "Fry")
+                            put("id", testData.fry.id)
+                            put("version", 0)
+                            put("admin", false)
+                            put("activated", true)
+                        }
+                        addJsonObject {
+                            put("userName", "Zoidberg")
+                            put("id", testData.zoidberg.id)
+                            put("version", 0)
+                            put("admin", false)
+                            put("activated", false)
+                        }
+                    }
+                }
+        }
 
-        client.post()
-            .uri("/api/users")
-            .sendJson(
-                """{
-                    "userName": "Leela",
-                    "admin": false
-                }"""
+        private fun setupPreconditions() = object : Preconditions(preconditionsInfra) {
+            val farnsworth = farnsworth()
+            val fry = fry()
+            val zoidberg = platformUser(
+                userName = "Zoidberg",
+                isAdmin = false,
+                activated = false
             )
-            .verifyOkAndJsonBody {
-                isEqualTo(
-                    JsonAssertions.json(
-                        """{
-                        userName: "Leela",
-                        id: "#{json-unit.any-number}",
-                        version: 0,
-                        admin: false,
-                        activated: false
-                    }"""
+        }
+    }
+
+    @Nested
+    @DisplayName("POST /api/users")
+    inner class CreateUser {
+
+        @Test
+        fun `should prohibit anonymous access`() {
+            sendRequest()
+                .exchange()
+                .expectStatus().isUnauthorized
+        }
+
+        @Test
+        @WithMockFryUser
+        fun `should prohibit regular user access`() {
+            sendRequest()
+                .exchange()
+                .expectStatus().isForbidden
+        }
+
+        @Test
+        @WithMockFarnsworthUser
+        fun `should create a new user`() {
+            setupPreconditions()
+            mockCurrentTime(timeService)
+
+            sendRequest()
+                .verifyOkAndJsonBodyEqualTo {
+                    put("userName", "Leela")
+                    put("id", "#{json-unit.any-number}")
+                    put("version", 0)
+                    put("admin", false)
+                    put("activated", false)
+                }
+
+            val createdUserId = aggregateTemplate.findAll<PlatformUser>()
+                .filter { it.userName == "Leela" }
+                .shouldBeSingle()
+                .id
+
+            aggregateTemplate.findSingle<UserActivationToken>()
+                .should {
+                    it.shouldBeEntityWithFields(
+                        UserActivationToken(
+                            userId = createdUserId!!,
+                            token = it.token,
+                            expiresAt = Instant.now()
+                        ),
+                        ignoredProperties = arrayOf(UserActivationToken::expiresAt)
                     )
-                )
+                    it.expiresAt.shouldBeEqualComparingTo(MOCK_TIME.plusSeconds(5 * 3600))
+                }
+        }
+
+        private fun sendRequest() = client
+            .post()
+            .uri("/api/users")
+            .sendJson {
+                put("userName", "Leela")
+                put("admin", false)
             }
 
-        val createdUserId = aggregateTemplate.findAll(PlatformUser::class.java)
-            .filter { it.userName == "Leela" }
-            .shouldHaveSize(1)
-            .first()
-            .id
-
-        aggregateTemplate.findAll(UserActivationToken::class.java)
-            .shouldHaveSize(1)
-            .first()
-            .should {
-                it.userId.shouldBe(createdUserId)
-                it.token.shouldNotBeNull()
-                it.expiresAt.shouldBeEqualComparingTo(MOCK_TIME.plusSeconds(5 * 3600))
+        private fun setupPreconditions() = object : Preconditions(preconditionsInfra) {
+            init {
+                farnsworth()
             }
+        }
     }
-
-    private fun setupPreconditions() = object : Preconditions(preconditionsInfra) {
-        val farnsworth = farnsworth()
-        val fry = fry()
-        val zoidberg = platformUser(
-            userName = "Zoidberg",
-            isAdmin = false,
-            activated = false
-        )
-    }
-
 }
