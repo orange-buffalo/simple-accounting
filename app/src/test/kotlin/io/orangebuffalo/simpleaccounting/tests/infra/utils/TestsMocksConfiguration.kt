0f7c877e1ea06a31b9c04fee488069c9af492dde
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-01-12 13:41:25 +1100

    tests: Removing deprecated `@SpyBean` usages #1593

--- app/src/test/kotlin/io/orangebuffalo/simpleaccounting/tests/infra/utils/TestsMocksConfiguration.kt
+++ app/src/test/kotlin/io/orangebuffalo/simpleaccounting/tests/infra/utils/TestsMocksConfiguration.kt
@@ -1,8 +1,10 @@
 package io.orangebuffalo.simpleaccounting.tests.infra.utils
 
+import com.nhaarman.mockitokotlin2.spy
 import io.orangebuffalo.simpleaccounting.infra.TimeService
-import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 /**
  * Setups common mocks for the tests to avoid creation of extra Spring Contexts
@@ -11,6 +13,12 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class TestsMocksConfiguration {
 
-    @SpyBean
-    lateinit var timeService: TimeService
+    /**
+     * See Spring limitation described in https://github.com/spring-projects/spring-framework/issues/33934#issuecomment-2512136161.
+     * Waiting for https://github.com/spring-projects/spring-framework/issues/33925 to be available - we then
+     * can convert this workaround with primary bean into a meta-annotation.
+     */
+    @Bean
+    @Primary
+    fun testTimeService(prodTimeService: TimeService) = spy(prodTimeService)
 }
