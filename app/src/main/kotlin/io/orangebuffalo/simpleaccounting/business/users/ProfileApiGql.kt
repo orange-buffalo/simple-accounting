f3ff41d443e76fcefac3af805e4d28bbf7428341
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-08-16 19:49:43 +1000

    build: Fixing GraphQL codegen for tests (#1904)

--- app/src/main/kotlin/io/orangebuffalo/simpleaccounting/business/users/ProfileApiGql.kt
+++ app/src/main/kotlin/io/orangebuffalo/simpleaccounting/business/users/ProfileApiGql.kt
@@ -3,26 +3,22 @@ package io.orangebuffalo.simpleaccounting.business.users
 import com.expediagroup.graphql.server.operations.Query
 import org.springframework.stereotype.Component
 
-data class UserProfile(
-    val test: String,
-)
-
 @Component
-class MyProfileQuery(
+class UserProfileQuery(
     private val platformUsersService: PlatformUsersService,
 ) : Query {
-    suspend fun myProfile(): MyProfileDto = platformUsersService
+    suspend fun userProfile(): UserProfileDto = platformUsersService
         .getCurrentUser()
         .mapToProfileDto()
 
-    data class MyProfileDto(
+    data class UserProfileDto(
         val userName: String,
         val documentsStorage: String?,
         val i18n: I18nSettingsDto
     )
 }
 
-private fun PlatformUser.mapToProfileDto() = MyProfileQuery.MyProfileDto(
+private fun PlatformUser.mapToProfileDto() = UserProfileQuery.UserProfileDto(
     userName = userName,
     documentsStorage = documentsStorage,
     i18n = I18nSettingsDto(
