208e37e1e1b92ccf21cbd4d53c52938ba53acee3
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-08-23 12:13:40 +1000

    feat: Implementing error handling and basic auth checks for GraphQL API (#1904)

--- app/src/main/kotlin/io/orangebuffalo/simpleaccounting/infra/graphql/GraphQlConfig.kt
+++ app/src/main/kotlin/io/orangebuffalo/simpleaccounting/infra/graphql/GraphQlConfig.kt
@@ -1,7 +1,10 @@
 package io.orangebuffalo.simpleaccounting.infra.graphql
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
+import com.expediagroup.graphql.generator.directives.KotlinDirectiveWiringFactory
+import com.expediagroup.graphql.generator.directives.KotlinSchemaDirectiveWiring
 import com.expediagroup.graphql.generator.federation.directives.ContactDirective
+import com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
 import com.expediagroup.graphql.server.Schema
 import org.springframework.stereotype.Component
 
@@ -13,3 +16,14 @@ import org.springframework.stereotype.Component
 @GraphQLDescription("GraphQL schema for Simple Accounting application")
 @Component
 class SaGraphQlSchema : Schema
+
+@Component
+class SaSchemaGeneratorHooks : SchemaGeneratorHooks {
+    private val directiveWiringFactory = KotlinDirectiveWiringFactory(
+        manualWiring = mapOf<String, KotlinSchemaDirectiveWiring>(
+            REQUIRED_AUTH_DIRECTIVE_NAME to RequiredAuthDirectiveWiring(),
+        )
+    )
+    override val wiringFactory: KotlinDirectiveWiringFactory
+        get() = directiveWiringFactory
+}
