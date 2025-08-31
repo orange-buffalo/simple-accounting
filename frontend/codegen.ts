cd0d353ea66e220afb804fae71bd9bcfe19cb6c0
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-08-31 21:22:27 +1000

    feat: Enabling GraphQl client on frontend (#1904)

---
+++ frontend/codegen.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: '../app/src/test/resources/api-schema.graphqls',
+  documents: ['src/**/*.vue'],
+  ignoreNoDocuments: true,
+  generates: {
+    './src/services/api/gql/': {
+      preset: 'client',
+      config: {
+        useTypeImports: true,
+      },
+      plugins: [],
+    },
+  },
+};
+
+export default config;
