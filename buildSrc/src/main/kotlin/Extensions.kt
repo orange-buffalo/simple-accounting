851201723ea1e085ae0d93610d3202bb0b8e503a
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-08-31 12:25:53 +1000

    tests: Improving tests performance by reducing Spring shutdown time

--- buildSrc/src/main/kotlin/Extensions.kt
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -1,13 +1,16 @@
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.TestDescriptor
 import org.gradle.api.tasks.testing.TestResult
+import java.time.Instant
 
 fun Project.printTestDescriptionDuringBuild(testDescriptor: TestDescriptor) {
-    logger.lifecycle("Running test: $testDescriptor")
+    logger.lifecycle("${Instant.now()}  [Test started] [$testDescriptor]")
 }
 
 fun Project.printTestResultDuringBuild(testDescriptor: TestDescriptor, testResult: TestResult) {
-    logger.lifecycle("Test $testDescriptor finished with result: $testResult")
+    logger.lifecycle("${Instant.now()}  [Test finished] [$testDescriptor] " +
+            "[time: ${testResult.endTime - testResult.startTime}ms] " +
+            "[result: $testResult]")
 }
 
 fun ifCi(action: () -> Unit) {
