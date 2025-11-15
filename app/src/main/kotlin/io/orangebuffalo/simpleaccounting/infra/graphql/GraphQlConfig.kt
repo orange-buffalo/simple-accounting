7545dad80b2a8c1a946296d7746ca6a6d33d6a42
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-11-15 16:03:28 +1100

    Fixing config

--- app/src/main/kotlin/io/orangebuffalo/simpleaccounting/infra/graphql/GraphQlConfig.kt
+++ app/src/main/kotlin/io/orangebuffalo/simpleaccounting/infra/graphql/GraphQlConfig.kt
@@ -1,22 +1,25 @@
 package io.orangebuffalo.simpleaccounting.infra.graphql
 
-import com.expediagroup.graphql.generator.GraphQLTypeResolver
+import com.expediagroup.graphql.generator.SchemaGenerator
 import com.expediagroup.graphql.generator.SchemaGeneratorConfig
-import com.expediagroup.graphql.generator.TopLevelNames
+import com.expediagroup.graphql.generator.TopLevelObject
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.generator.directives.KotlinDirectiveWiringFactory
 import com.expediagroup.graphql.generator.directives.KotlinSchemaDirectiveWiring
-import com.expediagroup.graphql.generator.execution.KotlinDataFetcherFactoryProvider
 import com.expediagroup.graphql.generator.federation.directives.ContactDirective
-import com.expediagroup.graphql.generator.hooks.NoopSchemaGeneratorHooks
 import com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
 import com.expediagroup.graphql.server.Schema
-import com.expediagroup.graphql.server.spring.GraphQLConfigurationProperties
-import graphql.schema.GraphQLEnumType
+import com.expediagroup.graphql.server.operations.Mutation
+import com.expediagroup.graphql.server.operations.Query
+import com.expediagroup.graphql.server.operations.Subscription
+import graphql.schema.GraphQLSchema
+import org.springframework.aop.framework.Advised
+import org.springframework.aop.support.AopUtils
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
-import java.util.Optional
+import java.util.*
+import kotlin.reflect.full.createType
 
 @ContactDirective(
     name = "Simple Accounting",
@@ -42,29 +45,42 @@ class SaSchemaGeneratorHooks : SchemaGeneratorHooks {
  * Configuration for GraphQL schema generation.
  */
 @Configuration
-class SaGraphQlSchemaConfig(
-    private val config: GraphQLConfigurationProperties
-) {
-    
+class SaGraphQlSchemaConfig {
+
+    /**
+     * Full copy of [com.expediagroup.graphql.server.spring.NonFederatedSchemaAutoConfiguration.schema] but with
+     * additional types added.
+     */
     @Bean
-    fun schemaConfig(
-        topLevelNames: Optional<TopLevelNames>,
-        hooks: Optional<SchemaGeneratorHooks>,
-        dataFetcherFactoryProvider: KotlinDataFetcherFactoryProvider,
-        typeResolver: GraphQLTypeResolver
-    ): SchemaGeneratorConfig = SchemaGeneratorConfig(
-        supportedPackages = config.packages,
-        topLevelNames = topLevelNames.orElse(TopLevelNames()),
-        hooks = hooks.orElse(NoopSchemaGeneratorHooks),
-        dataFetcherFactoryProvider = dataFetcherFactoryProvider,
-        introspectionEnabled = config.introspection.enabled,
-        additionalTypes = setOf(
-            GraphQLEnumType.newEnum()
-                .name("SaGrapQlErrorType")
-                .description("Defines the error types that can be returned in GraphQL errors. These error types are included in the `extensions.errorType` field of GraphQL errors.")
-                .value("NOT_AUTHORIZED", SaGrapQlErrorType.NOT_AUTHORIZED, "Indicates that the request requires authentication or the user is not authorized to perform the operation.")
-                .build()
-        ),
-        typeResolver = typeResolver
-    )
+    fun schema(
+        queries: Optional<List<Query>>,
+        mutations: Optional<List<Mutation>>,
+        subscriptions: Optional<List<Subscription>>,
+        schemaConfig: SchemaGeneratorConfig,
+        schemaObject: Optional<Schema>
+    ): GraphQLSchema {
+        val generator = SchemaGenerator(config = schemaConfig)
+        return generator.use {
+            it.generateSchema(
+                queries = queries.orElse(emptyList()).toTopLevelObjects(),
+                mutations = mutations.orElse(emptyList()).toTopLevelObjects(),
+                subscriptions = subscriptions.orElse(emptyList()).toTopLevelObjects(),
+                schemaObject = schemaObject.orElse(null)?.toTopLevelObject(),
+                additionalTypes = setOf(SaGrapQlErrorType::class.createType())
+            )
+        }
+    }
+
+    private fun List<Any>.toTopLevelObjects(): List<TopLevelObject> = this.map {
+        it.toTopLevelObject()
+    }
+
+    private fun Any.toTopLevelObject(): TopLevelObject = this.let {
+        val klazz = if (AopUtils.isAopProxy(it) && it is Advised) {
+            it.targetSource.target!!::class
+        } else {
+            it::class
+        }
+        TopLevelObject(it, klazz)
+    }
 }
