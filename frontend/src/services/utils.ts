ddb5fee897c62aa62ec3d7587926bb961bafd2e1
orange-buffalo <orange-buffalo@users.noreply.github.com>
2026-05-23 02:41:56 +0000

    chore: removing dead utils code

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,33 +1,6 @@
 import type { UnwrapRef, Ref, ComputedRef } from 'vue';
-import { computed, ref, watch } from 'vue';
-import type { HasOptionalId } from '@/services/api';
+import { computed, ref } from 'vue';
 import { useCurrentWorkspace } from '@/services/workspaces';
-
-export function findByIdOrEmpty<T extends HasOptionalId>(list: T[], targetItemId?: string): T | undefined {
-  return list
-    .find((it) => (it.id === targetItemId) || (!it.id && !targetItemId));
-}
-
-export function useValueLoadedByCurrentWorkspaceAndProp<T, P>(
-  propGetter: () => P | null | undefined,
-  valueLoader: (propValue: P, currentWorkspaceId: string) => Promise<UnwrapRef<T>>,
-) {
-  const value = ref<T | null>(null);
-  const loading = ref(true);
-  const { currentWorkspaceId } = useCurrentWorkspace();
-
-  watch(propGetter, async (propValue) => {
-    if (!propValue) return;
-    loading.value = true;
-    value.value = await valueLoader(propValue, currentWorkspaceId);
-    loading.value = false;
-  }, { immediate: true });
-
-  return {
-    value,
-    loading,
-  };
-}
 
 export function useValueLoadedByCurrentWorkspace<T>(
   valueLoader: (currentWorkspaceId: string) => Promise<UnwrapRef<T>>,
@@ -60,13 +33,4 @@ export function ensureDefined<T>(value: T | undefined | null): T {
     throw new Error('Value undefined');
   }
   return value;
-}
-
-export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
-
-/**
- * Checks if provided value is not undefined and not null.
- */
-export function hasValue<T>(param: T | undefined | null): boolean {
-  return param !== undefined && param !== null;
 }
